b67f754ca085a9b8930040e16ac3b0c15b94b421
shashi <shashidharngg@gmail.com>
2022-08-17 22:45:16 +0530

    created contact

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class AddressBook {public static void main(String[] args) {
+	System.out.println("Welcome to Address Book");
+	String firstName = "Xyz";
+	String lastName = "Ng";
+	String address = "Near SM Temple Bagalkot";
+	String city = "Bagalkot";
+	String state = "Karnatakaa";
+	int pinCode = 123456;
+	long mobileNumber = 8899221100l;
+	String emailId = "Abz@gmail.com";
+	System.out.println("Name 	     : "  + firstName + " " + lastName + "\n"
+            + "Address      : "  + address + "\n"
+            + "City         : "  + city + "\n"
+            + "State        : "  + state + "\n"
+            + "ZipCode      : "  + pinCode + "\n"
+            + "MobileNumber : "  + mobileNumber + "\n"
+            + "EmailId      : "  + emailId + "\n" );
+
+}
+	
+	
+
+}
